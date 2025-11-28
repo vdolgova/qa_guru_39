a5205e5d1040f2d8d7119da3acd6793d7eaf0650
VIRA <v.dolgovaa@gmail.com>
2025-11-28 20:48:04 +0300

    1version

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,29 @@
+package github;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SearchTests {
+
+    @Test
+    void shouldFindSelenideRepositoryStTheTop() {
+
+        //открыть главную страницу
+        open("https://github.com/");
+        // ввести в поле поиска selenide и нажать enter
+        $("[placeholder=\"Search or jump to...\"]").click();
+        $("[id=\"query-builder-test\"]").setValue("selenide").pressEnter();
+        // кликнуть на первый репозиторий из списка найденных
+        $$("[class=\"Box-sc-g0xbh4-0 JcuiZ\"]").first().$("a").click();
+        // .classname #id1 [] a[href=abc].red
+        // проверка: заголовок selenide/selenide
+        $("[href=\"/selenide/selenide\"]").shouldHave(Condition.text("selenide/selenide"));
+        sleep(5000);
+
+    }
+
+}
